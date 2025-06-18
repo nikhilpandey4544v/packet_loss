95fa2c54b1177cee590ccac1ccf2ccda7f1ade75
nikhilpandey4544v <shibuu18@gmail.com>
2025-06-18 16:27:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const title = document.getElementById("title");
+
+const colors = ["#ff4655", "#ff8a65", "#ffc107", "#4fc3f7", "#ab47bc"];
+let index = 0;
+
+setInterval(() => {
+  title.style.color = colors[index];
+  title.style.textShadow = `0 0 10px ${colors[index]}, 0 0 20px ${colors[index]}`;
+  index = (index + 1) % colors.length;
+}, 1500);
